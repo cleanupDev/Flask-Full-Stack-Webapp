16da924f74c4af604c80b4e68d406f73efd8587e
cleanupDev <philip.goddinger@gmail.com>
2023-07-29 21:36:37 +0200

    init backend work

--- backend/app.py
+++ backend/app.py
@@ -3,19 +3,6 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    return jsonify({'status': 'success',
-                    'message': 'Hello World'
-                    }), 200
-
-
-@app.route('/<name>')
-def hello_name(name):
-    return jsonify({'status': 'success',
-                    'message': 'Hello {}'.format(name)
-                    }), 200
-
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ models/user.py
@@ -0,0 +1,26 @@
+from dataclasses import dataclass
+
+@dataclass
+class User:
+    id: int
+    username: str
+    password: str
+    email: str
+    first_name: str
+    last_name: str
+    is_active: bool
+    is_authenticated: bool
+    
+    def get_id(self):
+        return self.id
+    
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "email": self.email,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "is_active": self.is_active,
+            "is_authenticated": self.is_authenticated
+        }
